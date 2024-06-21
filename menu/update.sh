bb0e619f788c69bd9ff7d44db72467bfdc8a9b9e
tanilink <rayyan.kontak@gmail.com>
2024-06-22 06:35:19 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LocalVersion=$(cat /root/versi)
-Version=$(curl -sS https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/versi | awk '{print $2}')
+Version=$(curl -sS https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/versi | awk '{print $2}')
 if [ $LocalVersion = $Version ]; then
 echo -e "Script Sudah Versi Terbaru..." | lolcat
 else
 echo -e "Update tersedia!!";
-wget -q -O updateyes.sh https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
+wget -q -O updateyes.sh https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
 rm -f updateyes.sh
 fi
