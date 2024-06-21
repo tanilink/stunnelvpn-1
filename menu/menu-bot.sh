509f21d0b761b9bb51fdba018ff9fc2f8fb12551
tanilink <rayyan.kontak@gmail.com>
2024-06-22 06:10:02 +0700

    Update menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -25,13 +25,13 @@ echo -e "\e[37;1m [04] • CREATE BOT NOTIFIKASI  \e[0m"
 echo -e "\e[31;1m [00] • GO BACK    \e[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[44;97;1m       RIFQI VPN TUNNELING         \e[0m"
+echo -e "\e[44;97;1m       TANILINK         \e[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -p " Just Input :  "  opt
 echo -e ""
 case $opt in
-01 | 1) clear ; rm -rf bot.sh && wget https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/bot.sh && chmod 777 bot.sh && ./bot.sh && systemctl restart cybervpn ;;
+01 | 1) clear ; rm -rf bot.sh && wget https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/bot.sh && chmod 777 bot.sh && ./bot.sh && systemctl restart cybervpn ;;
 02 | 2) clear ; systemctl stop cybervpn ;;
 03 | 3) clear ; systemctl start cybervpn  ;;
 04 | 4) clear ; chatid ;;
