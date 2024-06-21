90b5fa1ab42b64139b18ce0d19097b4fea6424f8
tanilink <rayyan.kontak@gmail.com>
2024-06-22 05:54:35 +0700

    Update acakdomain.sh

--- menu/acakdomain.sh
+++ menu/acakdomain.sh
@@ -4,11 +4,11 @@
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y
-DOMAIN=inject.cloud
+DOMAIN=kingvpn.my.id
 sub=tunnel-$(</dev/urandom tr -dc a-z | head -c4)
 SUB_DOMAIN=${sub}.${DOMAIN}
-CF_ID=mezzqueen293@gmail.com
-CF_KEY=e03f30d53ad7ec2ab54327baa5e2da5ab44f0
+CF_ID=hannaugo@gmail.com
+CF_KEY=e3341a6705e970eda3577f440d0cca6e3d682
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
