46d174832bc0d2a01729e4d985eeaedf1db91188
tanilink <rayyan.kontak@gmail.com>
2024-06-22 06:36:13 +0700

    Update upsc.sh

--- menu/upsc.sh
+++ menu/upsc.sh
@@ -30,28 +30,28 @@ rm -rf menu-bot
 rm -rf addhost
 # // Download Data Baru
 # // menu
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/dashboard "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/dashboard.sh" && chmod +x /usr/bin/dashboard
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-ssh.sh" &&  chmod +x /usr/bin/menu-ssh
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-noobzvpns "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-noobzvpns.sh" && chmod +x /usr/bin/menu-noobzvpns
-wget -q -O /usr/bin/trial-generator "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/trial/trial-generator.sh" && chmod +x /usr/bin/trial-generator
-wget -q -O /usr/bin/menu-ipsec "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/ipsec/menu-ipsec.sh" && chmod +x /usr/bin/menu-ipsec
-wget -q -O /usr/bin/system "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/system.sh" && chmod +x /usr/bin/system
-wget -q -O /usr/bin/about "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/about.sh" &&  chmod +x /usr/bin/about
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/Themes/setting.sh" && chmod +x /usr/bin/setting
-wget -q -O /usr/bin/upsc "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/upsc.sh" && chmod +x /usr/bin/upsc
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/restart.sh" && chmod +x /usr/bin/restart
-wget -q -O /usr/bin/menu-udp "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-udp.sh" && chmod +x /usr/bin/menu-udp
-wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-wget -q -O /usr/bin/addhost  "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/addhost.sh" && chmod +x /usr/bin/addhost
-#wget -qwget -q -O /usr/bin/menu-udp "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/menu/menu-udp.sh" && chmod +x /usr/bin/menu-udp
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/dashboard "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/dashboard.sh" && chmod +x /usr/bin/dashboard
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-ssh.sh" &&  chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+wget -q -O /usr/bin/menu-noobzvpns "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-noobzvpns.sh" && chmod +x /usr/bin/menu-noobzvpns
+wget -q -O /usr/bin/trial-generator "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/trial/trial-generator.sh" && chmod +x /usr/bin/trial-generator
+wget -q -O /usr/bin/menu-ipsec "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/ipsec/menu-ipsec.sh" && chmod +x /usr/bin/menu-ipsec
+wget -q -O /usr/bin/system "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/system.sh" && chmod +x /usr/bin/system
+wget -q -O /usr/bin/about "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/about.sh" &&  chmod +x /usr/bin/about
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/running.sh" && chmod +x /usr/bin/running
+wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/Themes/setting.sh" && chmod +x /usr/bin/setting
+wget -q -O /usr/bin/upsc "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/upsc.sh" && chmod +x /usr/bin/upsc
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/running.sh" && chmod +x /usr/bin/running
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/restart.sh" && chmod +x /usr/bin/restart
+wget -q -O /usr/bin/menu-udp "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-udp.sh" && chmod +x /usr/bin/menu-udp
+wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
+wget -q -O /usr/bin/addhost  "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/addhost.sh" && chmod +x /usr/bin/addhost
+#wget -qwget -q -O /usr/bin/menu-udp "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/menu/menu-udp.sh" && chmod +x /usr/bin/menu-udp
 echo -e "sleep 3 "
 clear
 echo -e "\e[32;1mSuccessfully\e[0m"
