b1b65858d6e7c20a494a91a29615af7366a9dd36
tanilink <rayyan.kontak@gmail.com>
2024-06-22 07:10:59 +0700

    Update bot.sh

--- botssh/bot.sh
+++ botssh/bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
 
-IZIN=$(curl -sS https://raw.githubusercontent.com/RifkyStoretuneling/REGISTER/main/IPVPS | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/tanilink/REGISTER/main/IPVPS | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
 else
@@ -19,7 +19,7 @@ apt install python3 python3-pip -y
 apt install sqlite3 -y
 cd /media/
 rm -rf cybervpn
-wget https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cybervpn.zip
+wget https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
@@ -53,7 +53,7 @@ END
 
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[44;97;1m        LUNATIC TUNNELING          \e[0m"
+echo -e "\e[44;97;1m        TANILINK          \e[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -103,61 +103,61 @@ echo -e "\e[44;97;1m        DOWNLOAD ASSET BOT         \e[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 
-wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/panelbot.sh" && chmod +x /usr/bin/panelbot
+wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/panelbot.sh" && chmod +x /usr/bin/panelbot
 
-wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/addnoobz.sh" && chmod +x /usr/bin/addnoobz
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
-wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/log-install.txt"
+wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/log-install.txt"
 
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/addtr.sh" && chmod +x /usr/bin/addtr
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/addtr.sh" && chmod +x /usr/bin/addtr
 
-wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/addws.sh" && chmod +x /usr/bin/addws
 
-wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/addss.sh" && chmod +x /usr/bin/addss
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
-wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-tr.sh" && chmod +x /usr/bin/cek-tr
 
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-vless.sh" && chmod +x /usr/bin/cek-vless
 
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-ws.sh" && chmod +x /usr/bin/cek-ws
 
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/del-vless.sh" && chmod +x /usr/bin/del-vless
 
-wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
-wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/deltr.sh" && chmod +x /usr/bin/deltr
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/delws.sh" && chmod +x /usr/bin/delws
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/delws.sh" && chmod +x /usr/bin/delws
 
-wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/delss.sh" && chmod +x /usr/bin/delss
 
-wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/renew-ss.sh" && chmod +x /usr/bin/renew-ss
 
-wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/renewtr.sh" && chmod +x /usr/bin/renewtr
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/renewtr.sh" && chmod +x /usr/bin/renewtr
 
-wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/renewvless.sh" && chmod +x /usr/bin/renewvless
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/renewvless.sh" && chmod +x /usr/bin/renewvless
 
-wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/renewws.sh" && chmod +x /usr/bin/renewws
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/renewws.sh" && chmod +x /usr/bin/renewws
 
-wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-mws.sh" && chmod +x /usr/bin/cek-mws
 
-wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
 
-wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-mss.sh" && chmod +x /usr/bin/cek-mss
 
-wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/RifkyStoretuneling/stunnelvpn/momok/botssh/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/tanilink/stunnelvpn-1/momok/botssh/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cp /tmp/var.txt /media/cybervpn
 
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[44;97;1m           RIFQI STORE             \e[0m"
+echo -e "\e[44;97;1m           TANILINK             \e[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
